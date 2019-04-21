bf95c8690482ec7863bfc25eb3529485369d7a91
saurabh kashyap <saurabhkashyap0001@gmail.com>
2019-04-22 03:45:05 +0530

    update links and other stuffs

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -403,7 +403,7 @@ class IndexPage extends React.Component {
   }
 
   render() {
-    let applySrc = 'https://docs.google.com/forms/d/e/1FAIpQLScmAqnoqT8j_XbQWgbQq_I8XaA2L9eRTPFcK2QJGJGm3hMtLA/viewform?usp=sf_link';
+    let applySrc = 'https://docs.google.com/forms/d/e/1FAIpQLSdi8hdNmfdRcgTSo_o1dvlyyk3xeO_HXqfNH4iQdepSrOhxHw/viewform?usp=sf_link';
     let imgSrc = '/button_submit.svg';
     let toastMsg = null;
     let toastColor = null;
@@ -445,7 +445,7 @@ class IndexPage extends React.Component {
         <LavaLampBg />
         <Container>
           <Title visible={this.state.mainVisible} subVisible={this.state.subVisible}>
-            Ambiensis
+            ambiensis
           </Title>
 
           <Logo src='/logo.png' visible={this.state.restVisible}/>
